79d6f03565c77e4c4231ddce3988747e58e9b2f8
xiaomengbai <1140083180@qq.com>
2026-03-16 16:34:34 +0800

    Files

---
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
@@ -0,0 +1,253 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Menu.h"
+#include "Components/Button.h"
+#include "MultiplayerSessionsSubsystem.h"
+#include "OnlineSessionSettings.h"
+#include "OnlineSubsystem.h"
+#include "TimerManager.h"
+
+void UMenu::MenuSetup(int32 NumOfPublicConnections, FString TypeOfMatch, FString LobbyPath)
+{
+	PathToLobby = FString::Printf(TEXT("%s?listen"), *LobbyPath);
+	
+	NumPublicConnections = NumOfPublicConnections;
+	MatchType = TypeOfMatch;
+	AddToViewport();
+	SetVisibility(ESlateVisibility::Visible);
+	SetFocus();
+
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		APlayerController* PlayerController = World->GetFirstPlayerController();
+		if (PlayerController)
+		{
+			FInputModeUIOnly InputModeData;
+			InputModeData.SetWidgetToFocus(TakeWidget());
+			InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+			PlayerController->SetInputMode(InputModeData);
+			PlayerController->SetShowMouseCursor(true);
+		}
+	}
+
+	UGameInstance* GameInstance = GetGameInstance();
+	if (GameInstance)
+	{
+		MultiplayerSessionsSubsystem = GameInstance->GetSubsystem<UMultiplayerSessionsSubsystem>();
+	}
+
+	if (MultiplayerSessionsSubsystem)
+	{
+		MultiplayerSessionsSubsystem->MultiplayerOnCreateSessionComplete.AddDynamic(this, &ThisClass::OnCreateSession);
+		MultiplayerSessionsSubsystem->MultiplayerOnFindSessionsComplete.AddUObject(this, &ThisClass::OnFindSessions);
+		MultiplayerSessionsSubsystem->MultiplayerOnJoinSessionComplete.AddUObject(this, &ThisClass::OnJoinSession);
+		MultiplayerSessionsSubsystem->MultiplayerOnDestorySessionComplete.AddDynamic(this, &ThisClass::OnDestorySession);
+		MultiplayerSessionsSubsystem->MultiplayerOnStartSessionComplete.AddDynamic(this, &ThisClass::OnStartSession);
+	}
+}
+
+bool UMenu::Initialize()
+{
+	if (!Super::Initialize())
+	{
+		return false;
+	}
+
+	if (HostButton)
+	{
+		HostButton->OnClicked.AddDynamic(this, &ThisClass::HostButtonClicked);
+	}
+	if (JoinButton)
+	{
+		JoinButton->OnClicked.AddDynamic(this, &ThisClass::JoinButtonClicked);
+	}
+	return true;
+}
+
+void UMenu::NativeDestruct()
+{
+	MenuTearDown();
+	Super::NativeDestruct();
+}
+
+void UMenu::OnCreateSession(bool bWasSuccessful)
+{
+	if (bWasSuccessful)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1,15.f,
+				FColor::Yellow,FString::Printf(TEXT("会话窗口创建成功! ")));
+		}
+
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			check(!PathToLobby.IsEmpty())
+			{
+				bool bIsSuccess = World->ServerTravel (PathToLobby);
+				if (bIsSuccess)
+				{
+				GEngine->AddOnScreenDebugMessage(-1,15.f,
+				FColor::Red,FString::Printf(TEXT("A! ")));
+				}
+				else
+				{
+					GEngine->AddOnScreenDebugMessage(-1,15.f,
+				FColor::Red,FString::Printf(TEXT("B!")));
+				}
+			}
+		}
+
+		IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+		IOnlineSessionPtr SessionInterface = Subsystem->GetSessionInterface();
+		FString Address;
+		SessionInterface->GetResolvedConnectString(NAME_GameSession, Address);
+		GEngine->AddOnScreenDebugMessage(-1,15.f,
+				FColor::Purple,FString::Printf(TEXT("连接的Address: %s"), *Address));
+		
+		
+	}
+	else
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1,15.f,
+				FColor::Red,FString::Printf(TEXT("创建会话窗口失败!"))
+			);
+		}
+		HostButton->SetIsEnabled(true);
+	}
+}
+
+void UMenu::OnFindSessions(const TArray<FOnlineSessionSearchResult>& SessionResults, bool bWasSuccessful)
+{
+	if (MultiplayerSessionsSubsystem == nullptr)
+	{
+		return;
+	}
+	for (auto Result : SessionResults)
+	{
+		FString Id = Result.GetSessionIdStr();
+		FString User = Result.Session.OwningUserName;
+		FString SettingsValue;
+		Result.Session.SessionSettings.Get(FName("MatchType"), SettingsValue);
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1,15.f,
+				FColor::Cyan,FString::Printf(TEXT("Id: %s, User: %s"), *Id, *User));
+		}
+		if (SettingsValue == MatchType)
+		{
+			if (GEngine)
+			{
+				GEngine->AddOnScreenDebugMessage(-1,15.f,
+					FColor::Cyan,FString::Printf(TEXT("Join Match Type: %s"), *MatchType));
+			}
+			MultiplayerSessionsSubsystem->JoinSession(Result);
+			return;
+		}
+	}
+	if (!bWasSuccessful || SessionResults.Num() == 0)
+	{
+		JoinButton->SetIsEnabled(true);
+	}
+}
+
+void UMenu::OnJoinSession(EOnJoinSessionCompleteResult::Type Result)
+{
+	if (Result != EOnJoinSessionCompleteResult::Success)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1,15.f,FColor::Red,
+				FString::Printf(TEXT("在Menu的OnJoinSession,加入会话失败!")));
+		}
+		JoinButton->SetIsEnabled(true);
+		return;
+	}
+	
+	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+	if (Subsystem)
+	{
+		IOnlineSessionPtr SessionInterface = Subsystem->GetSessionInterface();
+		if (SessionInterface.IsValid())
+		{
+			FString Address;
+			if (!SessionInterface->GetResolvedConnectString(NAME_GameSession, Address))
+			{
+				if (GEngine)
+				{
+					GEngine->AddOnScreenDebugMessage(-1,15.f,
+						FColor::Red,FString::Printf(TEXT("连接失败！")));
+				}
+				return;
+			}
+
+			if (Address.IsEmpty())
+			{
+				if (GEngine)
+				{
+					GEngine->AddOnScreenDebugMessage(-1,15.f,
+						FColor::Red,FString::Printf(TEXT("Address为空！")));
+				}
+				return;
+			}else
+			{
+				GEngine->AddOnScreenDebugMessage(-1,15.f,
+					FColor::Yellow,FString::Printf(TEXT("连接的Address: %s"), *Address));
+			}
+
+			APlayerController* PlayerController = GetGameInstance()->GetFirstLocalPlayerController();
+			if (PlayerController)
+			{
+				PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
+			}
+		}
+	}
+}
+
+void UMenu::OnDestorySession(bool bWasSuccessful)
+{
+}
+
+void UMenu::OnStartSession(bool bWasSuccessful)
+{
+}
+
+void UMenu::HostButtonClicked()
+{
+	HostButton->SetIsEnabled(false);
+	if (MultiplayerSessionsSubsystem)
+	{
+		MultiplayerSessionsSubsystem->CreateSession(NumPublicConnections, MatchType);
+	}
+}
+
+void UMenu::JoinButtonClicked()
+{
+	JoinButton->SetIsEnabled(false);
+	if (MultiplayerSessionsSubsystem)
+	{
+		MultiplayerSessionsSubsystem->FindSessions(20000);
+	}
+}
+
+void UMenu::MenuTearDown()
+{
+	RemoveFromParent();
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		APlayerController* PlayerController = World->GetFirstPlayerController();
+		if (PlayerController)
+		{
+			FInputModeGameOnly InputModeData;
+			PlayerController->SetInputMode(InputModeData);
+			PlayerController->SetShowMouseCursor(false);
+		}
+	}
+}
+
